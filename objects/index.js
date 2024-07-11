0e29ad2399a12f8dc284955e357bcc1e580e72dd
Tanjilur Rahman Jim <tanjilurrahman21@gmail.com>
2024-07-11 20:09:43 +0600

    Initial commit

---
+++ objects/index.js
@@ -0,0 +1,100 @@
+// objects are collections of key value pairs,
+// 
+
+
+
+// const circle = {
+//     radius:1,
+//     location: {
+//          x:1,
+//          y:1
+//      },
+//     isVisible: true,
+//     draw: function(){ //if a function is part of an object, it is called a method
+//         console.log('draw'); 
+//     }
+    
+
+// };
+
+
+// circle.draw();
+
+
+// Factory Funcions
+
+// let's create 2 circle objects
+
+// what if we had more methods thatn just draw ? 
+
+// const circle1 = {
+//     radius:1,
+//     location: {
+//          x:1,
+//          y:1
+//      },
+//     isVisible: true,
+//     draw: function(){ //if a function is part of an object, it is called a method
+//         console.log('draw'); 
+//     }
+    
+
+// };
+
+// factory funcion produce objects
+
+// function createCircle(radius,){
+
+//     return {
+//         radius, // if key and value are the same we can remove the value to make it shorter
+
+//         draw(){ 
+//             console.log('draw');
+        
+         
+        
+        
+//         }
+        
+    
+//     };
+
+// }
+
+// const circle1 = createCircle(1);
+// console.log(circle1)
+// const circle2 = createCircle(2);
+// console.log(circle2)
+    
+
+// let a = 'red'
+// let b = 'blue'
+
+// let temp = a;
+// a = b;
+// b = temp;
+
+// console.log(a);
+// console.log(b);
+
+// If else statements
+
+// let hour =7;
+// if (hour>=6 && hour <12) {
+//     console.log('Good morning');
+//     document.write('Good morning')
+
+// }
+// else if (hour >=12 && hour < 18){
+//     console.log('Good Afternoon')
+// }
+// else
+//     console.log('Good evening'); 
+
+// Switch case
+
+let role;
+
+switch (role){   // we add a variable
+
+}
